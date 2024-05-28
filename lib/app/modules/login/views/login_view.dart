560e9422a7c9a0fb7747f10feaa7d55ad67e0392
Nmmatakon <nmmatakon@gmail.com>
2024-05-28 10:57:06 +0100

    fixing login

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -23,78 +23,82 @@ class LoginView extends GetView<LoginController> {
           padding: EdgeInsets.symmetric(horizontal: 10.0),
           child: SizedBox(
             height: 60.0.hp,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                 Icon(
-                  Icons.person,
-                  size: 90.0.sp,
-                  color: Appcolors.redPrimary,
-                ),
-                SizedBox(
-                  height: 25.0.hp,
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround ,
-                    children: [
-                      TextfielCustomized(
-                          hintext: 'Entrez votre email',
-                          inconsPrefixed: Icons.email_outlined),
-                      SizedBox(
-                        child: Column(
-                          children: [
-                            TextfielCustomized(
-                              hintext: 'Entrez votre mot de pasee',
-                              inconsPrefixed: Icons.lock_outline,
-                              inconsSuffixed: Icons.remove_red_eye,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(top:5.0),
-                                  child: Text(
-                                    'Mot de passe oublié',
-                                    style: TextStyle(
-                                        fontFamily: 'Outfit',
-                                        fontSize: 14.0,
-                                        color: Appcolors.redPrimary,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(
+                    Icons.person,
+                    size: 90.0.sp,
+                    color: Appcolors.redPrimary,
                   ),
-                ),
-               
-                SizedBox(
-                    height: 15.0.hp,
-                    child: Column(
+                  SizedBox(
+                    height: 25.0.hp,
+                    child: const Column(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                         SizedBox(
-                          width: double.infinity,
-                          height: 55,
-                          child: ElevatedButton(
-                              onPressed: (){
-                                showDialog(
-                                  context: context,
-                                  builder:(BuildContext context){
-                                    return DialogCustomize(title: 'Bravo Bienvenue', describe: 'Vous vous etes connectés avec succés à wesero+');
-                                  }
-                                );
-                              }, child: Text("Connexion")),
-                        ),
-                        Text(
-                          "Pas de compte ?, S'inscrire",
-                          style: AppTheme.lightTheme.textTheme.bodySmall,
+                        TextfielCustomized(
+                            hintext: 'Entrez votre email',
+                            inconsPrefixed: Icons.email_outlined),
+                        SizedBox(
+                          child: Column(
+                            children: [
+                              TextfielCustomized(
+                                hintext: 'Entrez votre mot de pasee',
+                                inconsPrefixed: Icons.lock_outline,
+                                inconsSuffixed: Icons.remove_red_eye,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 5.0),
+                                    child: Text(
+                                      'Mot de passe oublié',
+                                      style: TextStyle(
+                                          fontFamily: 'Outfit',
+                                          fontSize: 14.0,
+                                          color: Appcolors.redPrimary,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ],
-                    )),
-              ],
+                    ),
+                  ),
+                  SizedBox(
+                      height: 15.0.hp,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          SizedBox(
+                            width: double.infinity,
+                            height: 55,
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return DialogCustomize(
+                                            title: 'Bravo Bienvenue',
+                                            describe:
+                                                'Vous vous etes connectés avec succés à wesero+');
+                                      });
+                                },
+                                child: Text("Connexion")),
+                          ),
+                          Text(
+                            "Pas de compte ?, S'inscrire",
+                            style: AppTheme.lightTheme.textTheme.bodySmall,
+                          ),
+                        ],
+                      )),
+                ],
+              ),
             ),
           ),
         ));
